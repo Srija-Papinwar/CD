3d59d3e90a27340385c5f9b19459345fef881704
Srija-Papinwar <papinwarsrija@gmail.com>
2022-03-28 12:04:14 +0530

    modified path of ova

--- scripts/deploy_ova.py
+++ scripts/deploy_ova.py
@@ -3,6 +3,6 @@ import urllib.parse
 import os
 from auto_loader import load_from_file
 config = load_from_file("auto_config")["deploy_ova"]
-command = "ovftool --powerOn -nw="+config["network"]+" -ds="+config["datastore"]+" -n="+config["vmname"]+" /home/venkatesh/CD/scripts/latestbuildfile.ova vi://"+config["user"]+":"+urllib.parse.quote(config["password"])+"@"+config["vcenterip"]+"/"+config["datacenter"]+"/host/"+config["cluster"]+"/"+config["host"]
+command = "ovftool --powerOn -nw="+config["network"]+" -ds="+config["datastore"]+" -n="+config["vmname"]+" /home/venkatesh/latestbuildfile.ova vi://"+config["user"]+":"+urllib.parse.quote(config["password"])+"@"+config["vcenterip"]+"/"+config["datacenter"]+"/host/"+config["cluster"]+"/"+config["host"]
 result = subprocess.run(command,shell = True ,stdout = subprocess.PIPE )
 print(result.stdout.decode())
