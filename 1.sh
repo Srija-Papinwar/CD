3d9c0a0361e766ccb442c26581c9e5d363b8ce20
Srija-Papinwar <papinwarsrija@gmail.com>
2022-03-30 08:57:13 +0530

    pipe1

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo $1 $2 $3 $4 $5 $6 $7 $8 $9 $10
-/home/venkatesh/home/ovftool/vmware-ovftool/ovftool  --noSSLVerify --powerOn -nw=$1 -ds=$2 -n=$3 $4  vi://$5:$6@$7/$8/host/$9/15.146.41.26
+echo $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}
+/home/venkatesh/home/ovftool/vmware-ovftool/ovftool  --noSSLVerify --powerOn -nw=$1 -ds=$2 -n=$3 $4  vi://$5:$6@$7/$8/host/$9/${10}
