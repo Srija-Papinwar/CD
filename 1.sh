aa47b052343b7f786ae6ab66e1796766ed387737
Srija-Papinwar <papinwarsrija@gmail.com>
2022-03-30 12:04:09 +0530

    dev_cd3

--- 1.sh
+++ 1.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-echo $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}
-/home/venkatesh/home/ovftool/vmware-ovftool/ovftool  --noSSLVerify --powerOn -nw=$1 -ds=$2 -n=$3 $4  vi://$5:$6@$7/$8/host/$9/${10}
+string="${6}"
+for(( p=0 ; p<${#6} ; p++)); do
+    c=${string:$p:1}
+    case "$c" in 
+        [-_.~a-zA-Z0-9] ) o="${c}";;
+        * )               printf -v o '%%%02x' "'$c"
+    esac
+    e+="${o}"
+done
+echo "${e}"
+/home/venkatesh/home/ovftool/vmware-ovftool/ovftool  --noSSLVerify --powerOn -nw=$1 -ds=$2 -n=$3 $4  vi://$5:${e}@$7/$8/host/$9/${10}
