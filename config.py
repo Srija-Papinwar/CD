736222e89e3b846a809188911a02193a0a355711
Srija-Papinwar <papinwarsrija@gmail.com>
2022-04-01 22:30:43 +0530

    config

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ openfile=open('auto_config.json', 'r')
 config = json.load(openfile)
 outfile= open("auto_config.json", "w")
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--deploy_ova", help="increase output verbosity")
-parser.add_argument("-f", "--fts", help="increas")
-parser.add_argument("-sd", "--pythonsdk", help="incre")
+parser.add_argument("-d", "--deploy_ova", help="increase output verbosity",default="none")
+parser.add_argument("-f", "--fts", help="increas",default="none")
+parser.add_argument("-sd", "--pythonsdk", help="incre",default="none")
 args = parser.parse_args()
 objects=str(args)[10:-2].split(",")
 for obj in objects :
     d=obj[:obj.find("=")].strip()
-    if obj[obj.find("=")+1:] != "Non":
+    if obj[obj.find("=")+1:].strip("'") != "none":
         li=obj[obj.find("=")+1:].split("?")
         for i in li:
             key,value=i.split(":")
